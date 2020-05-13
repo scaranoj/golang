7e7a221083c48602467a69fb94e75aff05211d3f
Jason Scarano <6853951+scaranoj@users.noreply.github.com>
2020-05-12 22:58:05 -0600

    add exercise 5

---
+++ udemy/exercise-5/exercise-5.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+type salami int
+
+//remember the order when declaring a variable with it's type
+//variable first, then it's type (i.e. "the variable x of type salami")
+
+var x salami
+var y int
+
+func main() {
+	//guess what the value will be here, and what is it called?
+	//Answer: 0, "zero value"
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	//the variable x is at package-level scope so this func sees it
+	x = 42
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+}
