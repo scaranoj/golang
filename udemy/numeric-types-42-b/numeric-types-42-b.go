0257b5c97c5ef2800f2730070df7792998b62d17
Jason Scarano <6853951+scaranoj@users.noreply.github.com>
2020-05-13 21:30:39 -0600

    add numeric types b

---
+++ udemy/numeric-types-42-b/numeric-types-42-b.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var y float64
+
+func main() {
+	x = 42
+	y = 42.34534
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+}
