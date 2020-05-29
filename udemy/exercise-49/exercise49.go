d001740ce380392acdcb2875e546f8d9f2392029
Jason Scarano <6853951+scaranoj@users.noreply.github.com>
2020-05-28 21:31:39 -0600

    add exercise 49

---
+++ udemy/exercise-49/exercise49.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 100
+	b := 200
+
+	fmt.Println("Is a == b?\t", (a == b))
+	fmt.Println("\nIs a <= b?\t", a <= b)
+	fmt.Println("\nIs a >= b?\t", a >= b)
+	fmt.Println("\nIs a != b?\t", a != b)
+	fmt.Println("\nIs a < b?\t", a < b)
+	fmt.Println("\nIs a > b?\t", a > b)
+
+}
