eb2c4c61bb1f7e61cc6063dff4fc324754961b4d
Jason Scarano <6853951+scaranoj@users.noreply.github.com>
2020-05-29 21:50:40 -0600

    add loop

---
+++ udemy/loop-56-a/loop-56-a.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//just remember, a for statement has an init, condition, post, and the curly braces {}
+	for i := 0; i <= 100; i++ {
+		fmt.Println(i)
+
+	}
+}
