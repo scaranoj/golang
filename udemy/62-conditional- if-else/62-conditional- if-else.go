90d8eaf4ee83dd85fc366d36cccde6a9755f15e3
Jason Scarano <scaranoj@vmware.com>
2020-06-29 11:17:57 -0600

    add else and else if

--- udemy/62-conditional- if-else/62-conditional- if-else.go
+++ udemy/62-conditional- if-else/62-conditional- if-else.go
@@ -5,11 +5,17 @@ import (
 )
 
 func main() {
-	x := 42
+	x := 44
 	if x == 40 {
 		fmt.Println("Our value was 40")
+	} else if x == 41 {
+		fmt.Println("Our value was 41")
+	} else if x == 42 {
+		fmt.Println("Our value was 42")
+	} else if x == 43 {
+		fmt.Println("Our value was 43")
 	} else {
-		fmt.Println("Our value was not 40")
+		fmt.Println("Our value was not 40, 41, 42, or 43")
 	}
 
 }
