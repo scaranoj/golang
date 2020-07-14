747adb44507a00624607c95aec3323bb685707f0
Jason Scarano <scaranoj@vmware.com>
2020-07-14 17:38:59 -0600

    update comments

--- udemy/101a-anonymous-struct/101a-anonymous-struct.go
+++ udemy/101a-anonymous-struct/101a-anonymous-struct.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 )
-
-type person struct {
-	last  string
-	first string
-	age   int
-}
+//notice I didn't bother creating a new struct type here?
+//it's because I'm only using a struct in one little area
+//and want to keep this code lightweight and clean and want to avoid 
+//creating extraneous types where I don't need them
+//therefore, I'm just creating an anonymous struct under main
 
 func main() {
-	p1 := person{
+	p1 := struct {
+		last  string
+		first string
+		age   int
+	}{
 		last:  "Bond",
 		first: "James",
 		age:   32,
